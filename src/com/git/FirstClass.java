365f3f7d3b8d3dfd0b169793bf70e669c3c3c507
karthikGitkar12345 <karthik.s110893@gmail.com>
2022-08-06 12:30:09 +0530

    vivek

--- src/com/git/FirstClass.java
+++ src/com/git/FirstClass.java
@@ -1,6 +1,9 @@
 package com.git;
 
 public class FirstClass {
+	private void vivek() {
+		System.out.println("Empid : 123456");
+	}
 
 	public void Name() {
 		System.out.println("Karthik Selladurai");
@@ -13,12 +16,13 @@ public class FirstClass {
 	public void Location() {
 		System.out.println("Thuraipakkam");
 	}
-	
+
 	public static void main(String[] args) {
 		FirstClass f = new FirstClass();
 		f.Name();
 		f.Course();
 		f.Location();
+		f.vivek();
 	}
 
 }
